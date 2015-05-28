6c8f5b15e33238fa6372600d87e924756e577bfc
PJ Passalacqua <pjpassalacqua@Daniels-MacBook-Pro.local>
2015-05-28 13:59:21 -0400

    Added a function to reformat the input from a file

--- word_frequency.py
+++ word_frequency.py
@@ -1,7 +1,22 @@
 import re
 
 
+# Functions
+
+
+# Takes a list of strings, joins them, and removes any punctuation.
+def reformat(text):
+    file_text = ' '.join(file_text)
+    # Remove whitespace.
+    file_text = re.sub(r'[^A-Za-z1-9 ]', "", file_text)
+    file_text = re.sub(r'[ *]', " ", file_text)
+
+
 # Read file.
+with open('sample.txt') as f:
+    file_text = f.readlines()
+
+file_text = reformat(file_text)
 # Create Dictionary.
 # Keep top 20 words from dictionary.
 # Print out the top 20 words.
